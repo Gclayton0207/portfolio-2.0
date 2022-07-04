d4268669212950382889854eaeece2a264d8c879
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-07-04 19:33:50 -0300

    feat: inserindo componente sobre

--- src/components/sobre/Sobre.jsx
+++ src/components/sobre/Sobre.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import './sobre.css'
+import React from "react";
+import "./sobre.css";
+import Eu from "../../assets/Sobre.png";
+import { FaAward } from "react-icons/fa";
+import { FiGitCommit } from "react-icons/fi";
+import { TbWorld } from "react-icons/tb";
 
 const Sobre = () => {
   return (
-    <section id='sobre'>
-      sobre
-    </section>
-  )
-}
+    <section id="sobre">
+      <h2>Sobre mim</h2>
+      <div className="container sobre__container">
+        <div className="sobre__mim"></div>
+        <div className="sobre__mim imagem">
+          <img src={Eu} alt="foto Giovanni Clayton" />
+        </div>
+        <div className="sobre__conteudo">
+          <div className="sobre__cards">
+            <article className="sobre__card">
+              <FiGitCommit className="sobre__icone" />
+              <h5>Commits</h5>
+              <small>+ de 300 commits feitos</small>
+            </article>
+            <article className="sobre__card">
+              <FaAward className="sobre__icone" />
+              <h5>Mais que pronto</h5>
+              <small>Conceito final no curso da Resilia Educação</small>
+            </article>
+            <article className="sobre__card">
+              <TbWorld className="sobre__icone" />
+              <h5>Projetos</h5>
+              <small>+10 projetos feito</small>
+            </article>
+          </div>
+          <p>Olá me chamo Giovanni Clayton Dias Alves, sou desenvolvedor full stack, graduado em Análise e Desenvolvimento de sistemas, pela Universidade Anhanguera, formado em desenvolvimento web na Resilia Educação busco uma oportunidade de trabalho na área de desenvolvimento, como desenvolvedor web junior.</p>
 
-export default Sobre
+          <p>Apaixonado por tecnologia em busca de recolocação no mercado de trabalho,venho da área de suporte técnico e espero poder usar as habilidades adquiridas nesse tempo, como trabalho em equipe, resolução de problemas e treinamento de funcionários, da melhor forma possível.</p>
+
+           <p>Em minha antiga função como Field Engineer na Unisys, eu era responsável por solucionar vários problemas técnicos para a equipe, ajudar na manutenção do servidor e instalar uma ampla variedade de equipamentos. Enquanto trabalhava lá, ajudei no desenvolvimento e implantação de novas práticas do departamento.</p>
+           <a href="#contato" className="btn btn-primary">Vamos conversar!</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Sobre;
