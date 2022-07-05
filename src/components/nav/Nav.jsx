34ffdea06f8bfaed28ec40bd9457993c68b38cba
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-07-05 09:49:51 -0300

    fix: hook nav home

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import {useState,React} from 'react'
 import './nav.css'
 import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
@@ -6,10 +7,10 @@ import {MdHomeRepairService} from 'react-icons/md'
 
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#home')
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#sobre" onClick={() => setActiveNav('#sobre')} className={activeNav === '#sobre' ? 'active' : ''}><AiOutlineUser /></a>
       <a href="#experiencia" onClick={() => setActiveNav('#experiencia')} className={activeNav === '#experiencia' ? 'active' : ''}><BiBook /></a>
       <a href="#servicos" onClick={() => setActiveNav('#servicos')} className={activeNav === '#servicos' ? 'active' : ''}><MdHomeRepairService /></a>
