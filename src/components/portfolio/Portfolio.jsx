a135a6158ff1ad2db681855eb923512d1ef96570
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-07-07 14:17:27 -0300

    feat: inserindo componente portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,12 +1,176 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
 
 const Portfolio = () => {
   return (
-    <div>
-      portfolio
-    </div>
-  )
-}
+    <section id="portfolio">
+      <h5>Meus projetos recentes</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img
+              src="https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao1!"
+              alt="imagem de uma previa do projeto"
+            />
+          </div>
+          <h3>Titulo do projeto</h3>
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="btn"
+          >
+            Github
+          </a>
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-primary"
+          >
+            Pagina
+          </a>
+        </article>
+      </div>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img
+              src="https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao2!"
+              alt="imagem de uma previa do projeto"
+            />
+          </div>
+          <h3>Titulo do projeto</h3>
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="btn"
+          >
+            Github
+          </a>
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-primary"
+          >
+            Pagina
+          </a>
+        </article>
+      </div>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img
+              src="https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao3!"
+              alt="imagem de uma previa do projeto"
+            />
+          </div>
+          <h3>Titulo do projeto</h3>
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="btn"
+          >
+            Github
+          </a>
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-primary"
+          >
+            Pagina
+          </a>
+        </article>
+      </div>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img
+              src="https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao4!"
+              alt="imagem de uma previa do projeto"
+            />
+          </div>
+          <h3>Titulo do projeto</h3>
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="btn"
+          >
+            Github
+          </a>
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-primary"
+          >
+            Pagina
+          </a>
+        </article>
+      </div>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img
+              src="https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao5!"
+              alt="imagem de uma previa do projeto"
+            />
+          </div>
+          <h3>Titulo do projeto</h3>
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="btn"
+          >
+            Github
+          </a>
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-primary"
+          >
+            Pagina
+          </a>
+        </article>
+      </div>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img
+              src="https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao6!"
+              alt="imagem de uma previa do projeto"
+            />
+          </div>
+          <h3>Titulo do projeto</h3>
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="btn"
+          >
+            Github
+          </a>
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-primary"
+          >
+            Pagina
+          </a>
+        </article>
+      </div>
+      
+    </section>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
