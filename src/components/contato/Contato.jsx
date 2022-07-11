9033e6291a002cb4b4f4b9dd86621a8b9f3f7bec
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-07-11 11:00:47 -0300

    feat: Instalando e configurando EmailJS para envio de emails pelo formulario

--- src/components/contato/Contato.jsx
+++ src/components/contato/Contato.jsx
@@ -2,8 +2,31 @@ import React from "react";
 import "./contato.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { BsWhatsapp } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contato = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_zbudvgp",
+        "template_zglcyia",
+        form.current,
+        "nD0YYmdq4LU0eC-7v"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
     <section id="contato">
       <h5>Entrar em contato</h5>
@@ -12,7 +35,7 @@ const Contato = () => {
       <div className="container contato__container">
         <div className="contato__opcoes">
           <article className="contato__opcao">
-            <MdOutlineEmail className="contato__opcao-icon"/>
+            <MdOutlineEmail className="contato__opcao-icon" />
             <h4>Email</h4>
             <h5>giovanni.gcda@gmail.com</h5>
             <a
@@ -36,11 +59,18 @@ const Contato = () => {
             </a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="nome" placeholder="Nome Completo" required />
           <input type="email" name="email" placeholder="Seu email" required />
-          <textarea name="mensagem" placeholder="Sua mensagem" rows="7" required></textarea>
-          <button type="submit" class="btn btn-primary">Enviar Mensagem</button>
+          <textarea
+            name="mensagem"
+            placeholder="Sua mensagem"
+            rows="7"
+            required
+          ></textarea>
+          <button type="submit" class="btn btn-primary">
+            Enviar Mensagem
+          </button>
         </form>
       </div>
     </section>
