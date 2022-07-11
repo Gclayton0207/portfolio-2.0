329045e1917787f90ce2504190e3c45288f12ef7
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-07-11 09:21:46 -0300

    refactor: usando metodo array.map para exibir os projetos

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,175 +1,92 @@
 import React from "react";
 import "./portfolio.css";
 
+const data = [
+  {
+    id: 1,
+    image:
+      "https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao1!",
+    Titulo: "Titulo do projeto",
+    github: "https://github.com/",
+    pagina: "https://github.com/",
+  },
+  {
+    id: 2,
+    image:
+      "https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao2!",
+    Titulo: "Titulo do projeto",
+    github: "https://github.com/",
+    pagina: "https://github.com/",
+  },
+  {
+    id: 3,
+    image:
+      "https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao3!",
+    Titulo: "Titulo do projeto",
+    github: "https://github.com/",
+    pagina: "https://github.com/",
+  },
+  {
+    id: 4,
+    image:
+      "https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao4!",
+    Titulo: "Titulo do projeto",
+    github: "https://github.com/",
+    pagina: "https://github.com/",
+  },
+  {
+    id: 5,
+    image:
+      "https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao5!",
+    Titulo: "Titulo do projeto",
+    github: "https://github.com/",
+    pagina: "https://github.com/",
+  },
+  {
+    id: 6,
+    image:
+      "https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao6!",
+    Titulo: "Titulo do projeto",
+    github: "https://github.com/",
+    pagina: "https://github.com/",
+  },
+];
+
 const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>Meus projetos recentes</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img
-              src="https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao1!"
-              alt="imagem de uma previa do projeto"
-            />
-          </div>
-          <h3>Titulo do projeto</h3>
-          <div className="portfolio__item-cta">
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn"
-          >
-            Github
-          </a>
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn btn-primary"
-          >
-            Pagina
-          </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img
-              src="https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao2!"
-              alt="imagem de uma previa do projeto"
-            />
-          </div>
-          <h3>Titulo do projeto</h3>
-          <div className="portfolio__item-cta">
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn"
-          >
-            Github
-          </a>
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn btn-primary"
-          >
-            Pagina
-          </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img
-              src="https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao3!"
-              alt="imagem de uma previa do projeto"
-            />
-          </div>
-          <h3>Titulo do projeto</h3>
-          <div className="portfolio__item-cta">
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn"
-          >
-            Github
-          </a>
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn btn-primary"
-          >
-            Pagina
-          </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img
-              src="https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao4!"
-              alt="imagem de uma previa do projeto"
-            />
-          </div>
-          <h3>Titulo do projeto</h3>
-          <div className="portfolio__item-cta">
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn"
-          >
-            Github
-          </a>
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn btn-primary"
-          >
-            Pagina
-          </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img
-              src="https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao5!"
-              alt="imagem de uma previa do projeto"
-            />
-          </div>
-          <h3>Titulo do projeto</h3>
-          <div className="portfolio__item-cta">
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn"
-          >
-            Github
-          </a>
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn btn-primary"
-          >
-            Pagina
-          </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img
-              src="https://via.placeholder.com/480/4db5ff/FFFFFF/?text=Em+construcao6!"
-              alt="imagem de uma previa do projeto"
-            />
-          </div>
-          <h3>Titulo do projeto</h3>
-          <div className="portfolio__item-cta">
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn"
-          >
-            Github
-          </a>
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn btn-primary"
-          >
-            Pagina
-          </a>
-          </div>
-        </article>
-        </div>
+        {data.map(({ id, image, titulo, github, pagina }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={titulo} />
+              </div>
+              <h3>{titulo}</h3>
+              <div className="portfolio__item-cta">
+                <a
+                  href={github}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn"
+                >
+                  Github
+                </a>
+                <a
+                  href={pagina}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn btn-primary"
+                >
+                  Pagina
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 };
